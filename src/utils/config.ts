671a9fe754d520473d8b460f2975f250eb3384e4
pukochka <tema.puko@gmail.com>
2025-06-19 15:09:01 +0300

    update view orders n fixes

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,8 +4,8 @@ export default {
   url: 'https://vpn-telegram.com/api/v1/key-activate/',
   authUrl: 'https://api.bot-t.com/v1/',
 
-  bot_id: getQueryParam('bot_id') || 886,
-  public_key: getQueryParam('public_key') || 'd7c3dfaae085801e284e3534004e818d',
+  bot_id: getQueryParam('bot_id'),
+  public_key: getQueryParam('public_key'),
 
   dark: false,
   // dark: window.Telegram.WebApp.colorScheme === 'dark',
